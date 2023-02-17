f9ceb91b98381bb5aec48d04de4770d6d5301921
THANASIS PETSAS <thanospets98@gmail.com>
2023-02-17 12:28:00 +0200

    inputs and css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [cells, setCells] = useState(["a", "b", "c"]);
+
+  const handleChangeText = (event, idx) => {
+    setCells(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <section className="App-header">
+        {cells.map((cell, idx) => {
+          return (
+            <div key={idx} className="cells">
+              <input
+                onChange={(event) => handleChangeText(event.target.value, idx)}
+                value={cell}
+              ></input>
+            </div>
+          );
+        })}
+      </section>
+    </main>
   );
 }
 
